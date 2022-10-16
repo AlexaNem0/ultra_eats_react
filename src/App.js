44d6c08f957050f9ce472a32c18f4a67794e0200
AlexaNem0 <vikalyeksyeyev@gmail.com>
2022-10-16 10:07:26 -0500

    FoodItem added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TopCards from "./components/TopCards";
+import FoodItem from "./components/FoodItem";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <TopCards />
+      <FoodItem />
     </div>
   );
 }
